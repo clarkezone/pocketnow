a6dd2ec806b77eec7cd60ac6cb2a98ec61dd837a
James Clarke <james@clarkezone.net>
2022-12-18 22:22:40 +0000

    Scrape the URL

--- src/pocketnow-test/UnitTest1.cs
+++ src/pocketnow-test/UnitTest1.cs
@@ -1,14 +1,23 @@
 namespace pocketnow_test;
 
+// RUn this with dotnet test -v n
+
 public class TestImageService
 {
+    public TestImageService() {
+System.Diagnostics.Trace.Listeners.Add(new System.Diagnostics.DefaultTraceListener());
+    }
+
     [Fact]
     public async void Test1()
     {
         ImageService s = new ImageService();
         var result = await s.GetLatestImage();
-        System.Diagnostics.Debug.WriteLine(result);
-        Assert.Equal(4, 4);
+        //Console.WriteLine("Result:" + result);
+	Console.WriteLine("Contains 17dcfaa4c117197c91a2eb1eeb28fd8d:" + result.Contains("17dcfaa4c117197c91a2eb1eeb28fd8d".ToString()));
+	var linkParser = new Regex(@"\b(?:https?://|www\.)\S+\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+	foreach(Match m in linkParser.Matches(result))
+		    Console.WriteLine(m.Value);
     }
 
       [Fact]
