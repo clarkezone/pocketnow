6154ea26a807eb2ec902db6acba287b04def74ab
James Clarke <james@clarkezone.net>
2023-04-05 19:07:49 +0000

    Refactor cosmos

--- src/geocache/go.mod
+++ src/geocache/go.mod
@@ -3,10 +3,11 @@ module github.com/clarkezone/geocache
 go 1.19
 
 require (
+	github.com/Azure/azure-sdk-for-go/sdk/data/azcosmos v0.3.3
 	github.com/clarkezone/boosted-go v0.0.0-20230317133643-96730c3ae442
-	github.com/fatih/color v1.15.0
+	github.com/google/uuid v1.3.0
 	github.com/sirupsen/logrus v1.9.0
-	github.com/spf13/cobra v1.6.1
+	github.com/spf13/cobra v1.7.0
 	github.com/spf13/viper v1.15.0
 	google.golang.org/grpc v1.54.0
 	google.golang.org/protobuf v1.30.0
@@ -15,13 +16,12 @@ require (
 require (
 	github.com/Azure/azure-sdk-for-go v63.2.0+incompatible // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.0.0 // indirect
-	github.com/Azure/azure-sdk-for-go/sdk/data/azcosmos v0.3.3 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/internal v1.0.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/fatih/color v1.15.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
@@ -40,7 +40,7 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
 	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
 	google.golang.org/genproto v0.0.0-20230403163135-c38d8f061ccd // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
--- src/geocache/pkg/cosmosdbbackend/cosmosbackend.go
+++ src/geocache/pkg/cosmosdbbackend/cosmosbackend.go
@@ -11,11 +11,6 @@ import (
 	"github.com/clarkezone/geocache/pkg/geocacheservice"
 )
 
-var endpoint = "<azure_cosmos_uri>"
-var key = "<azure_cosmos_primary_key"
-var databasename = ""
-var containername = ""
-
 type DAOSample struct {
 	ID           string `json:"id"`
 	PartitionID  string `json:"partitionid"`
@@ -25,6 +20,29 @@ type DAOSample struct {
 	Lon          float64
 	BatteryState string
 	Timestamp    string
+}
+
+type CosmosDAL struct {
+	endpoint string
+	key      string
+	client   *azcosmos.Client
+}
+
+func CreateCosmosDAL(endpoint, key string) (*CosmosDAL, error) {
+	cred, err := azcosmos.NewKeyCredential(key)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := azcosmos.NewClientWithKey(endpoint, cred, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &CosmosDAL{
+		endpoint: endpoint,
+		key:      key,
+		client:   client,
+	}, nil
 }
 
 func getThing(lo *geocacheservice.Location) (ds DAOSample) {
@@ -44,60 +62,23 @@ func getThing(lo *geocacheservice.Location) (ds DAOSample) {
 	return item
 }
 
-func auth() (*azcosmos.DatabaseClient, *azcosmos.ContainerClient) {
-
-	cred, err := azcosmos.NewKeyCredential(key)
-	if err != nil {
-		log.Fatal("Failed to create a credential: ", err)
-	}
-
-	// Create a CosmosDB client
-	client, err := azcosmos.NewClientWithKey(endpoint, cred, nil)
-	if err != nil {
-		log.Fatal("Failed to create Azure Cosmos DB client: ", err)
-	}
-
-	// Create database client
-	databaseClient, err := client.NewDatabase("<databaseName>")
-	if err != nil {
-		log.Fatal("Failed to create database client:", err)
-	}
-
-	// Create container client
-	containerClient, err := client.NewContainer("<databaseName>", "<containerName>")
-	if err != nil {
-		log.Fatal("Failed to create a container client:", err)
-	}
-
-	return databaseClient, containerClient
-}
-
-func createDatabase(client *azcosmos.Client, databaseName string) error {
-	//	databaseName := "adventureworks"
-
-	// sets the name of the database
+func (dal *CosmosDAL) CreateDatabase(databaseName string) error {
 	databaseProperties := azcosmos.DatabaseProperties{ID: databaseName}
 
-	// creating the database
 	ctx := context.TODO()
-	_, err := client.CreateDatabase(ctx, databaseProperties, nil)
+	_, err := dal.client.CreateDatabase(ctx, databaseProperties, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	return nil
 }
 
-func createContainer(client *azcosmos.Client, databaseName, containerName, partitionKey string) error {
-	//	databaseName = "adventureworks"
-	//	containerName = "customer"
-	//	partitionKey = "/customerId"
-
-	databaseClient, err := client.NewDatabase(databaseName) // returns a struct that represents a database
+func (dal *CosmosDAL) CreateContainer(databaseName, containerName, partitionKey string) error {
+	databaseClient, err := dal.client.NewDatabase(databaseName) // returns a struct that represents a database
 	if err != nil {
 		log.Fatal("Failed to create a database client:", err)
 	}
 
-	// Setting container properties
 	containerProperties := azcosmos.ContainerProperties{
 		ID: containerName,
 		PartitionKeyDefinition: azcosmos.PartitionKeyDefinition{
@@ -105,7 +86,6 @@ func createContainer(client *azcosmos.Client, databaseName, containerName, parti
 		},
 	}
 
-	// Setting container options
 	throughputProperties := azcosmos.NewManualThroughputProperties(400) //defaults to 400 if not set
 	options := &azcosmos.CreateContainerOptions{
 		ThroughputProperties: &throughputProperties,
@@ -122,37 +102,12 @@ func createContainer(client *azcosmos.Client, databaseName, containerName, parti
 	return nil
 }
 
-func createItem(client *azcosmos.Client, databaseName, containerName, partitionKey string, item any) error {
-	//	databaseName = "adventureworks"
-	//	containerName = "customer"
-	//	partitionKey = "1"
-	/*
-		item = struct {
-			ID           string `json:"id"`
-			CustomerId   string `json:"customerId"`
-			Title        string
-			FirstName    string
-			LastName     string
-			EmailAddress string
-			PhoneNumber  string
-			CreationDate string
-		}{
-			ID:           "1",
-			CustomerId:   "1",
-			Title:        "Mr",
-			FirstName:    "Luke",
-			LastName:     "Hayes",
-			EmailAddress: "luke12@adventure-works.com",
-			PhoneNumber:  "879-555-0197",
-		}
-	*/
-	// Create container client
-	containerClient, err := client.NewContainer(databaseName, containerName)
+func (dal *CosmosDAL) CreateItem(databaseName, containerName, partitionKey string, item any) error {
+	containerClient, err := dal.client.NewContainer(databaseName, containerName)
 	if err != nil {
 		return fmt.Errorf("failed to create a container client: %s", err)
 	}
 
-	// Specifies the value of the partiton key
 	pk := azcosmos.NewPartitionKeyString(partitionKey)
 
 	b, err := json.Marshal(item)
