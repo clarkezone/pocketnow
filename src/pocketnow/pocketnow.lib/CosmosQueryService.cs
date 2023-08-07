f3a2e9229201b267db7bd8755e1eb3c3b170a1f7
James Clarke <james@clarkezone.net>
2023-08-07 13:03:45 -0700

    Make basic test work

--- src/pocketnow/pocketnow.lib/CosmosQueryService.cs
+++ src/pocketnow/pocketnow.lib/CosmosQueryService.cs
@@ -1,17 +1,22 @@
 
 using Microsoft.Azure.Cosmos;
 
-namespace PocketBase
+namespace pocketnow
 {
     public class CosmosQueryService
     {
-        public void Connect()
+        public void Connect(string Endpoint, string key)
         {
             using CosmosClient client = new(
-                accountEndpoint: Environment.GetEnvironmentVariable("COSMOS_ENDPOINT")!,
-                authKeyOrResourceToken: Environment.GetEnvironmentVariable("COSMOS_KEY")!
-
+                accountEndpoint: Endpoint,
+                authKeyOrResourceToken: key
             );
+            //authKeyOrResourceToken: Environment.GetEnvironmentVariable("COSMOS_KEY")!
+            var databaseName = "pocketnow";
+            var containerName = "geocache";
+            var partitionKey = "1";
+            var db = client.GetDatabase(databaseName);
+            db.GetContainer(containerName);
 
         }
     }
