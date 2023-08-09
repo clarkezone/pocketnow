a7353f151ee8a90987cb308e82e45e6448e39784
James Clarke <james@clarkezone.net>
2023-08-08 18:05:01 -0700

    Add real query

--- src/pocketnow/pocketnow.Tests/UnitTest1.cs
+++ src/pocketnow/pocketnow.Tests/UnitTest1.cs
@@ -5,7 +5,16 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-        pocketnow.lib.Class1 c = new();
-        c.GetType();
+        pocketnow.CosmosQueryService cosmosQueryService = new ();
+        cosmosQueryService.Connect("https://pocketnow.documents.azure.com:443",
+         "78FDsSHV7CNb0cfOnG7TvpFzjjFYhHsx5p0eolg8ZVweVFMfMQUcZGSNqLB72RNwHO9sKHzy7Mo0ACDbU3wLtQ==");
+    }
+    [Fact]
+    public void Test2()
+    {
+        pocketnow.CosmosQueryService cosmosQueryService = new ();
+        var thing = cosmosQueryService.Connect("https://pocketnow.documents.azure.com:443",
+         "78FDsSHV7CNb0cfOnG7TvpFzjjFYhHsx5p0eolg8ZVweVFMfMQUcZGSNqLB72RNwHO9sKHzy7Mo0ACDbU3wLtQ==");
+         cosmosQueryService.GetGeoLog(thing);
     }
 }
