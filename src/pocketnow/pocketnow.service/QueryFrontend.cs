3fa70aa0aae1291a6f3bddf7a92b4b6981d07c7a
James Clarke <james@clarkezone.net>
2023-08-16 18:28:44 -0700

    Cleanup

--- src/pocketnow/pocketnow.service/QueryFrontend.cs
+++ src/pocketnow/pocketnow.service/QueryFrontend.cs
@@ -21,23 +21,5 @@ namespace pocketnow
             return group;
         }
 
-// This is not used
-        private static void Configure()
-        {
-            var builder = WebApplication.CreateBuilder();
-            builder.Services.AddControllers();
-            var app = builder.Build();
-            app.UseHttpsRedirection();
-            var pbhost = Environment.GetEnvironmentVariable("HOST") ?? string.Empty;
-            var cosmosendpoint = Environment.GetEnvironmentVariable("COSMOS_URL") ?? string.Empty;
-            var cosmoskey = Environment.GetEnvironmentVariable("COSMOS_KEY") ?? string.Empty;
-            var serviceurl = Environment.GetEnvironmentVariable("SERVICEURL") ?? string.Empty;
-
-            Console.WriteLine($"Username: {cosmosendpoint}");
-            Console.WriteLine($"Password: {cosmoskey}");
-
-            pocketnow.CosmosQueryService cosmosQueryService = new();
-            cosmosQueryService.Connect(cosmosendpoint, cosmoskey);
-        }
     }
 }
